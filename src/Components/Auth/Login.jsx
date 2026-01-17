9400271bb8efb806d20557463b99335839306cf5
Uzrakhan <uzrakhan539@gmail.com>
2026-01-17 13:21:18 +0530

    Changed error to err

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
      const handleGoogleSignIn = async () => {
         try {
         await googleSignIn();
-        } catch (error) {
+        } catch (err) {
             handleErrors(err);
         }
     };
